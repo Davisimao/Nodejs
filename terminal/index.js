a5ece7ce93654a22a3261c19c984401ae2183094
Davi Sanches Simão <davisimao.dev@gmail.com>
2024-05-27 16:41:56 -0300

    Readline

---
+++ terminal/index.js
@@ -0,0 +1,10 @@
+const { stdin, stdout } = require("node:process")
+const readLine = require("node:readline")
+
+const rl = readLine.createInterface({ input: process.stdin, output: process.stdout })
+
+
+rl.question("Qual o seu nome? ", (nome) => {
+  rl.write("ola " + nome)
+  rl.close()
+})
