0aec7a7fbef7c2c44f7eae561a86438ed0ce432c
Davi Sanches Simão <davisimao.dev@gmail.com>
2024-06-18 20:18:25 -0300

    form e post

--- Aplicações WEB/04 -html-ejs/src/server.js
+++ Aplicações WEB/04 -html-ejs/src/server.js
@@ -2,6 +2,7 @@ const express = require("express")
 const path = require("node:path")
 
 const app = express()
+const memory = []
 
 const PORT = 3000
 
@@ -9,13 +10,29 @@ const PORT = 3000
 app.set('view engine', 'ejs')
 app.set("views", path.join(__dirname, "views"),)
 
+app.use(express.urlencoded({ extended: true }))
+
 app.get("/", (req, res) => {
   const title = "homepage"
   const message = "mensagem dinamica do EJS"
 
 
-  res.render('index', { title, message })
+  res.render('form', { title, message })
 
+})
+
+
+
+app.post("/register", (req, res) => {
+  const username = req.body.username
+  const password = req.body.password
+
+  memory.push({ username, password })
+  res.redirect("/usuarios")
+})
+
+app.get("/usuarios", (req, res) => {
+  res.render("users", { users: memory })
 })
 
 app.listen(PORT, () => {
