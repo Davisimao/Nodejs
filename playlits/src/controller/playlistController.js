b9928c8ceb265657e86b880e9404f4c4c3d650aa
Davi Sanches Simão <davisimao.dev@gmail.com>
2024-07-12 21:36:23 -0300

    movendo o arr playlist pra fora do controller

--- playlits/src/controller/playlistController.js
+++ playlits/src/controller/playlistController.js
@@ -1,21 +1,4 @@
-const playlists = [
-  {
-    id: 1,
-    musics: [
-    ],
-    name: "Morning Vibes",
-    tags: ["chill", "morning", "relax"],
-  },
-  {
-    id: 2,
-    musics: [
-      { id: 3, title: "Track X", artist: "Artist A", album: "Vingando a paixão" },
-    ],
-    name: "Workout Hits",
-    tags: ["energetic", "workout", "pump"],
-  }
-]
-
+const { playlists } = require("../playlists")
 
 module.exports = {
   //GET /playlist
---
+++ playlits/src/playlists.js
@@ -0,0 +1,17 @@
+const playlists = [
+  {
+    id: 1,
+    musics: [],
+    name: "Morning Vibes",
+    tags: ["chill", "morning", "relax"],
+  },
+  {
+    id: 2,
+    musics: [
+      { id: 3, title: "Track X", artist: "Artist A", album: "Vingando a paixão" },
+    ],
+    name: "Workout Hits",
+    tags: ["energetic", "workout", "pump"],
+  }
+];
+exports.playlists = playlists;
