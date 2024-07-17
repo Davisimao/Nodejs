82cf5b2f70bf056589a952a8d5dc69a545ba4939
Davi Sanches Simão <davisimao.dev@gmail.com>
2024-07-17 15:32:37 -0300

    verificando e não decodificando

--- Aplicações WEB/12-jwt/src/middleware/auth-middleware.js
+++ Aplicações WEB/12-jwt/src/middleware/auth-middleware.js
@@ -11,7 +11,7 @@ const authMiddleware = (req, res, next) => {
   const token = authHeader.split(" ")[1]
 
   try {
-    const decodedToken = jwt.decode(token, secretkey)
+    const decodedToken = jwt.verify(token, secretkey)
     const user = users.find(user => user.username === decodedToken.username)
     if (!user) {
       return res.status(401).json({ message: "invalid user" })
