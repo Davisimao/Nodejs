1e71aa4f8a2982f06356d2aa87bc17aa1bd63217
Davi Sanches Simão <davisimao.dev@gmail.com>
2024-07-16 22:09:39 -0300

    criando um jwt

--- Aplicações WEB/12-jwt/src/routes/auth.js
+++ Aplicações WEB/12-jwt/src/routes/auth.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const users = require("../models/users")
-
+const jwt = require("jsonwebtoken")
+const secretkey = "jacagay-!123456"
 const authRouter = express.Router()
 
 authRouter.post("/register", (req, res) => {
@@ -14,6 +15,19 @@ authRouter.post("/register", (req, res) => {
   users.push(newUser)
 
   res.status(201).json(newUser)
+})
+
+authRouter.post("/login", (req, res) => {
+  const { username, password } = req.body
+
+  const user = users.find(user => user.username === username)
+
+  if (!user || user.password !== password) {
+    return res.json(401).json({ message: "not found!" })
+  }
+  const token = jwt.sign({ message: "testando payload" }, secretkey, { expiresIn: "1h" })
+  res.json({ token })
+
 })
 
 module.exports = authRouter
